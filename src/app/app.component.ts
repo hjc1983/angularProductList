fb013d3b241f2bcbabb0278babfac4bd4ea6c437
rc <13476236+hjc1983@users.noreply.github.com>
2021-01-10 10:40:15 +1000

    CICD Replace token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  isProd = environment.production;
+  apiEndpoint = environment.apiEndpoint;
   title = 'myAngular';
 }
